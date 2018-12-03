181ee852dafa54842a3ffaced6d712c541f648fc
JuYoungYoo <juy5790@outlook.com>
2018-12-03 19:23:59 +0900

    Initial commit  + spring boot  + JPA, H2  + Lombok

---
+++ src/main/java/com/juyoung/webserver/controller/RestController.java
@@ -0,0 +1,13 @@
+package com.juyoung.webserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @GetMapping
+    public String hello(){
+        return "Hello World";
+    }
+
+}
